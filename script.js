b93b36434876e040f3cc242b67ae71326bc28ee7
Matheus Michelan <matheusm.michelan@hotmail.com>
2024-05-09 23:51:20 -0300

    texto

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('mostrar-mensagem').addEventListener('click', function () {
+    document.getElementById('mensagem').innerHTML = 'Olá! Esta é uma mensagem de boas-vindas!';
+});
